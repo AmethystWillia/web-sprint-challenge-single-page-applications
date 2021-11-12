ae22895c7d7c44d60f0a66eb0cd34ea01db1ecdb
Amethyst Williams <zmithic@gmail.com>
2021-11-12 17:54:08 -0600

    Added and passed: order starts disabled, can fill inputs + filled inputs enable order, incorrect inputs summon errors +do not enable order, submit form

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -8,7 +8,7 @@ describe('Pizza Form App', () => {
     const orderBtn = () => cy.get('button[id="order-button"]');
     const nameInput = () => cy.get('input[id="name-input"]');
     const sizeInput = () => cy.get('select[id="size-dropdown"]');
-    const sauceInput = () => cy.get('input[id="sauce-radio"]');
+    const sauceInput = () => cy.get('[type="radio"]');
     const pepperoniInput = () => cy.get('input[name="pepperoni"]');
     const sausageInput = () => cy.get('input[name="sausage"]');
     const canBaconInput = () => cy.get('input[name="canBacon"]');
@@ -36,5 +36,76 @@ describe('Pizza Form App', () => {
         canBaconInput().should('exist');
         onionsInput().should('exist');
         specialTextInput().should('exist');
+    });
+
+    describe('Filling out inputs', () => {
+        it('Can navigate to site', () => {
+            cy.url().should('include', 'localhost');
+        });
+
+        it('Order button starts disabled', () => {
+            orderBtn().should('be.disabled');
+        });
+
+        it('Can fill out inputs + Order enables when filled properly', () => {
+            nameInput()
+                .should('have.value', '')
+                .type('Amy')
+                .should('have.value', 'Amy')
+            sizeInput()
+                .should('have.value', '')
+                .select('Small')
+                .should('have.value', 'Small')
+            sauceInput()
+                .should('not.be.checked')
+                .check('Garlic Ranch')
+                .should('have.value', 'Garlic Ranch')
+            pepperoniInput()
+                .should('not.be.checked')
+                .check()
+                .should('be.checked')
+            sausageInput()
+                .should('not.be.checked')
+                .check()
+                .should('be.checked')
+            canBaconInput()
+                .should('not.be.checked')
+                .check()
+                .should('be.checked')
+            onionsInput()
+                .should('not.be.checked')
+                .check()
+                .should('be.checked')
+            specialTextInput()
+                .should('have.value', '')
+                .type('Beep')
+                .should('have.value', 'Beep')
+
+            orderBtn()
+                .should('not.be.disabled');
+        });
+
+        it('Incorrect inputs do not enable submit + Errors show up', () => {
+            nameInput().type('q');
+            sizeInput().select('Small').select('');
+
+            cy.contains('must').should('exist'); // Check for name error
+            cy.contains("We can't").should('exist'); // Check for pizza error
+
+            orderBtn().should('be.disabled');
+        });
+    });
+
+    describe('Order a pizza', () => {
+        it('Can submit a pizza', () => {
+            nameInput().type('Amy');
+            sizeInput().select('Small');
+            sauceInput().check('Original Red');
+            specialTextInput().type('Testing...');
+
+            orderBtn().click();
+
+            cy.contains('You Ordered Pizza!!').should('exist');
+        });
     });
 });
